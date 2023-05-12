b80cf8391c2bc20dcff33b136b396234843dad45
wormz30 <kevinle@me.com>
2023-05-12 10:08:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-//const express = require('express');
-//const fetch = require('node-fetch');
-import fetch from 'node-fetch';
+
 import express from 'express';
+import fetch from 'node-fetch';
 
 const app = express();
 const port = 3000;
